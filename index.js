dc1df96c4a55506da664aadf9a434933cde9ba6f
Mahalingam <kmahalingam@mitre.org>
2019-11-19 15:32:16 -0500

    Initial implementation of CLI Post operation:

---
+++ index.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+
+const axios = require('axios');
+const path = require('path');
+const fs = require('fs-extra');
+const program = require('commander');
+
+let input;
+
+program
+    .name('icaredata-client')
+    .usage('<path-to-messages> [options]')
+    .option('-o, --out <out>', 'the path to the output folder', path.join('.', 'out'))
+    .arguments('<path-to-messages>')
+    .action(function (pathToMessages) {
+        input = pathToMessages;
+    })
+    .parse(process.argv);
+
+// Check that input folder is specified
+if (!input) {
+    console.error('Missing path to messages folder.');
+    program.help();
+}
+
+// Try to parse the files from the input directory
+let files;
+try {
+    files = fs.readdirSync(input, 'utf8');
+} catch (e) {
+    console.error('Invalid path to messages folder.');
+    program.help();
+}
+
+// Collect the message files and the config file
+const data = { config: null, messages: [] };
+for (const file of files) {
+    const fileContent = fs.readFileSync(path.join(input, file), 'utf8');
+    if (fileContent) {
+        if (file === 'config.json') {
+            data.config = JSON.parse(fileContent);
+        } else if (file.endsWith('.json')) {
+            data.messages.push(fileContent);
+        }
+    }
+}
+
+const apiGateway = axios.create({
+    baseURL: data.config.baseURL,
+    timeout: data.config.timeout,
+});
+
+for (const message of data.messages) {
+    apiGateway.post('/DSTU2/$process-message', message)
+        .then(r => console.log('Success'))
+        .catch(e => console.error(e.message));
+}
