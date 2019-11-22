ccb960cdc18117cda6e7094f60313a80d5a8b1d9
Kathy Li <lli@mitre.org>
2019-11-22 13:24:45 -0500

    Add linting

--- index.js
+++ index.js
@@ -12,28 +12,28 @@ program
     .usage('<path-to-messages> [options]')
     .option('-o, --out <out>', 'the path to the output folder', path.join('.', 'out'))
     .arguments('<path-to-messages>')
-    .action(function (pathToMessages) {
-        input = pathToMessages;
+    .action(function(pathToMessages) {
+      input = pathToMessages;
     })
     .parse(process.argv);
 
 // Check that input folder is specified
 if (!input) {
-    console.error('Missing path to messages folder.');
-    program.help();
+  console.error('Missing path to messages folder.');
+  program.help();
 }
 
 // Try to parse the files from the input directory
 let files;
 try {
-    files = fs.readdirSync(input, 'utf8');
+  files = fs.readdirSync(input, 'utf8');
 } catch (e) {
-    console.error('Invalid path to messages folder.');
-    program.help();
+  console.error('Invalid path to messages folder.');
+  program.help();
 }
 
 // Collect the message files and the config file
-const data = { config: null, messages: [] };
+const data = {config: null, messages: []};
 for (const file of files) {
     const filePath = path.join(input, file);
     if (fs.statSync(filePath).isDirectory()) continue;
@@ -43,23 +43,37 @@ for (const file of files) {
             data.config = JSON.parse(fileContent);
         } else if (file.endsWith('.json')) {
             data.messages.push({fileName: file, fileContent});
-        }
     }
+  }
 }
 
 if (!data.config) {
-    console.error('No config.json in messages folder.');
-    program.help();
+  console.error('No config.json in messages folder.');
+  program.help();
 }
 
 if (data.messages.length === 0) {
-    console.error('No messages found in messages folder.');
-    program.help();
+  console.error('No messages found in messages folder.');
+  program.help();
 }
 
+// TODO: remove this once OAuth is behind api gateway
+const httpsAgent = new https.Agent({
+  rejectUnauthorized: false, // Turn off ssl verification
+});
+const oauth = axios.create({
+  httpsAgent,
+  baseURL: 'https://localhost:9000',
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/x-www-form-urlencoded',
+  },
+});
+
+
 const apiGateway = axios.create({
-    baseURL: data.config.baseURL,
-    timeout: data.config.timeout,
+  baseURL: data.config.baseURL,
+  timeout: data.config.timeout,
 });
 
 for (const message of data.messages) {
