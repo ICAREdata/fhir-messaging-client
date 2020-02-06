aaf5ec345714ae609bca612aa38ca11bb7912f0a
Rob Dingwell <rob@robdingwell.org>
2020-02-06 13:36:36 -0500

    Refactored client into a class that can be reused to support workflows other than reading from directory.  Reading from directory functionality has been moved to the cli script

--- index.js
+++ index.js
@@ -2,28 +2,49 @@
 
 const program = require('commander');
 const Client = require('./src/Client.js');
+const fs = require('fs')
+const path = require('path')
+
+function readConfigFile(configFile) {
+  return JSON.parse(fs.readFileSync(configFile));
+}
+
 
 let input;
 program
-    .name('icaredata-client')
-    .usage('<path-to-messages> [options]')
-    .option(
-        '-i, --identity-file <identityFilePath>',
-        'the path to the private jwk',
-    )
-    .option('-c, --client-id <clientId>', 'the client id')
-    .arguments('<path-to-messages>')
-    .action(function(pathToMessages) {
-      input = pathToMessages;
-    })
-    .parse(process.argv);
+  .name('icaredata-client')
+  .usage('<path-to-messages> [options]')
+  .option(
+    '-c, --config <configFilePath>',
+    'the path to the configuration file',
+  )
+  .arguments('<path-to-messages>')
+  .action(function (pathToMessages) {
+    input = pathToMessages;
+  })
+  .parse(process.argv);
 
-const identityFilePath = program.identityFile || process.env.ICD_PRIVATE_JWK;
-const clientId = program.clientId || process.env.ICD_CLIENT_ID;
-
+// create a new client
+// loop over directory of files 
+// collect results 
 try {
-  const client = new Client(input, identityFilePath, clientId);
-  client.send();
+  const config = readConfigFile(program.config);
+  const client = new Client(config);
+  client.authorize().then(() => {
+    const files = fs.readdirSync(input, 'utf8');
+    for (const file of files) {
+      const filePath = path.join(input, file);
+      if (fs.statSync(filePath).isDirectory()) continue;
+      const fileContent = fs.readFileSync(filePath, 'utf8');
+      if (fileContent) {
+        if (file.endsWith('.json')) {
+          client.processMessage(fileContent)
+            .then((r) => console.log(`${file} - Success!`))
+            .catch((e) => console.error(`${file} - ${e.message}`));
+        }
+      }
+    }
+  });
 } catch (e) {
   console.error(e);
   program.help();
--- src/Client.js
+++ src/Client.js
@@ -1,169 +1,98 @@
+
 const https = require('https');
 const axios = require('axios');
-const path = require('path');
-const fs = require('fs');
 const uuidv4 = require('uuid/v4');
 const {JWS} = require('node-jose');
-const querystring = require('querystring');
 
 /**
  * A Client to be used to post Messages to a FHIR server after negotiating
  * authentication with an OAuth server.
  */
 module.exports = class Client {
+
   /**
    * Create the Client.
-   * @param {string} input The input path containing FHIR messages.
-   * @param {string} identityFilePath The path to the private JWK.
+   * @param {string} JWK The input path containing FHIR messages.
+   * @param {string} url The path to the private JWK.
    * @param {string} clientId The client id to use for authentication.
    */
-  constructor(input, identityFilePath, clientId) {
-    // Check that input folder is specified
-    if (input) {
-      this.input = input;
-    } else {
-      throw new Error('Missing path to messages folder.');
-    }
-
-    // Check that a private jwk exists
-    if (identityFilePath) {
-      try {
-        this.identityFile = require(identityFilePath);
-      } catch (e) {
-        throw new Error(`Invalid path to private JWK: ${e}`);
-      }
-    } else {
-      throw new Error('Missing private JWK.');
-    }
-
-    // Check that a client id exists
-    if (clientId) {
-      this.clientId = clientId;
-    } else {
-      throw new Error('Missing client ID.');
-    }
-
-    // Try to parse the files from the input directory.
-    try {
-      this.files = fs.readdirSync(this.input, 'utf8');
-    } catch (e) {
-      throw new Error(`Invalid path to messages folder: ${e}`);
-    }
-
-    // Collect the message files and the config file
-    this.data = {config: null, messages: []};
-    for (const file of files) {
-      const filePath = path.join(this.input, file);
-      if (fs.statSync(filePath).isDirectory()) continue;
-      const fileContent = fs.readFileSync(filePath, 'utf8');
-      if (fileContent) {
-        if (file === 'config.json') {
-          this.data.config = JSON.parse(fileContent);
-        } else if (file.endsWith('.json')) {
-          this.data.messages.push({fileName: file, fileContent});
-        }
-      }
-    }
-
-    if (this.data.messages.length === 0) {
-      throw new Error('No messages found in messages folder.');
-    }
-
-    if (!this.data.config) {
-      throw new Error('No config.json in messages folder.');
-    }
-
-    if (!this.data.config.baseURL) {
-      throw new Error('No baseURL specified in config.json.');
-    }
-
-    if (!this.data.config.timeout) {
-      throw new Error('No timeout specified in config.json.');
-    }
+  constructor(config) {
+    this.config = config;
+    this.jwk = this.config.jwk;
+    this.clientId = this.config.clientId;
 
     this.apiGateway = axios.create({
-      baseURL: this.data.config.baseURL,
-      timeout: this.data.config.timeout,
+      baseURL: this.config.baseURL,
+      timeout: this.config.timeout
     });
   }
 
-  /**
-   * Authenticate and send Messages to FHIR server.
-   */
-  send() {
-    const tokenEndpointPromise = this.apiGateway.get(
+  setBearerToken(token) {
+    this.apiGateway.defaults.headers.common['Authorization'] =
+      `Bearer ${token}`;
+  }
+
+
+  getTokenUrl() {
+    return this.apiGateway.get(
         '/.well-known/smart-configuration',
-    )
-        .then((r) => r.data.token_endpoint)
-        .catch((e) => {
-          throw new Error(`Request for token endpoint failed: ${e.message}`);
-        });
+      ).then((r) => r.data.token_endpoint)
+  }
 
-    const httpsAgent = new https.Agent({
-      rejectUnauthorized: false, // Turn off ssl verification
-    });
 
-    const oauthPromise = tokenEndpointPromise
-        .then((tokenEndpoint) => axios.create({
-          httpsAgent,
-          baseURL: tokenEndpoint,
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/x-www-form-urlencoded',
-          },
-        }));
+  generateClientAssetion(tokenEndpoint) {
 
     const options = {
       compact: true,
-      alg: 'RS256',
+      alg: 'RS384',
       fields: {
-        kid: this.identityFile.kid,
+        kid: this.jwk.kid,
         typ: 'JWT',
-      },
+      }
     };
 
-    const assertionPromise = tokenEndpointPromise
-        .then((tokenEndpoint) => {
-          const content = JSON.stringify({
-            iss: this.clientId,
-            sub: this.clientId,
-            aud: tokenEndpoint,
-            exp: Math.floor(Date.now()/1000) + 300,
-            jti: uuidv4(),
-          });
-          return JWS.createSign(
-              options,
-              this.identityFile,
-          ).update(content).final();
-        })
-        .catch((e) => {
-          throw new Error(`Failed to create assertion: ${e.message}`);
-        });
-
-    const tokenPromise = Promise.all([assertionPromise, oauthPromise])
-        .then(([assertion, oauth]) => {
-          const body = querystring.stringify({
-            client_assertion: assertion,
-            client_assertion_type:
-              'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
-            grant_type: 'client_credentials',
-            scope: 'system/$process-message',
-          });
-          return oauth.post('', body);
-        })
-        .then((r) => r.data.access_token)
-        .catch((e) => {
-          throw new Error(`Failed to obtain access token: ${e.message}`);
-        });
-
-    tokenPromise.then((token) => {
-      this.apiGateway.defaults.headers.common['Authorization'] =
-          `Bearer ${token}`;
-
-      for (const message of this.data.messages) {
-        this.processMessage(message);
-      }
+    const content = JSON.stringify({
+      iss: this.clientId,
+      sub: this.clientId,
+      aud: tokenEndpoint,
+      exp: Math.floor(Date.now() / 1000) + 300,
+      jti: uuidv4()
     });
+
+    const assertion = JWS.createSign(
+      options,
+      this.identityFile,
+    ).update(content).final();
+
+    return {
+      client_assertion: assertion,
+      client_assertion_type: 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
+      grant_type: 'client_credentials',
+      scope: 'system/$process-message'
+    };
+
+  }
+
+  authorize() {
+    return this.getTokenUrl().then((tokenEndpoint) => {
+      let rejectUnauthorized = false
+      if (this.config.ssl_strict == false) {
+        rejectUnauthorized = true;
+      }
+      const httpsAgent = new https.Agent({
+        rejectUnauthorized: rejectUnauthorized // Turn off ssl verification
+      });
+      const assertion = this.generateClientAssetion(tokenEndpoint);
+      axios.create({
+        httpsAgent,
+        baseURL: tokenEndpoint,
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      }).post('', assertion).then((response) => this.setBearerToken(response.data.access_token));
+
+    })
   }
 
   /**
@@ -171,8 +100,7 @@ module.exports = class Client {
    * @param {object} message The FHIR Message to send
    */
   processMessage(message) {
-    this.apiGateway.post('/DSTU2/$process-message', message.fileContent)
-        .then((r) => console.log(`${message.fileName} - Success!`))
-        .catch((e) => console.error(`${message.fileName} - ${e.message}`));
+    return this.apiGateway.post('/DSTU2/$process-message', message)
   };
-};
+
+}
