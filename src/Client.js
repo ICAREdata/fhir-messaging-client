4d7716ab7fd9216dfb8dc45c75a85433f6fc3109
rdingwell <rob.dingwell@gmail.com>
2020-03-11 14:02:07 -0400

    Fixing issues.  Cleint assertion creation required to be async functi… (#4)
    * Fixing issues.  Cleint assertion creation required to be async function to allow for the signature to be created.  This also required restructuring the authorization method to deal with the new promise return value.  Added ability to configure the aud parameter of the jwt, currently keycloak is expecting the base relm url instead of the token url. Properly formated authorization request with urlencoded body
    
    * fixing issues with tests casued by changes in Client promise scopiong
    
    * Fix lint errors
    
    Co-authored-by: Mahalingam <kmahalingam@mitre.org>

--- src/Client.js
+++ src/Client.js
@@ -1,6 +1,7 @@
 const https = require('https');
 const axios = require('axios');
 const uuidv4 = require('uuid/v4');
+const querystring = require('querystring');
 const {
   JWS,
 } = require('node-jose');
@@ -14,7 +15,6 @@ module.exports = class Client {
     this.config = config;
     this.jwk = this.config.jwk;
     this.clientId = this.config.clientId;
-
     this.apiGateway = axios.create({
       baseURL: this.config.baseURL,
       timeout: this.config.timeout,
@@ -24,8 +24,6 @@ module.exports = class Client {
   setBearerToken(token) {
     this.apiGateway.defaults.headers.common['Authorization'] =
       `Bearer ${token}`;
-
-    console.log(this.apiGateway.defaults.headers.common['Authorization']);
   }
 
   getTokenUrl() {
@@ -34,8 +32,7 @@ module.exports = class Client {
     ).then((r) => r.data.token_endpoint);
   }
 
-
-  generateClientAssertion(tokenEndpoint, jti) {
+  async generateClientAssertion(tokenEndpoint, jti) {
     const options = {
       compact: true,
       alg: 'RS384',
@@ -48,12 +45,12 @@ module.exports = class Client {
     const content = JSON.stringify({
       iss: this.clientId,
       sub: this.clientId,
-      aud: tokenEndpoint,
+      aud: this.config.aud || tokenEndpoint,
       exp: Math.floor(Date.now() / 1000) + 300,
       jti: jti || uuidv4(),
     });
 
-    const assertion = JWS.createSign(
+    const assertion = await JWS.createSign(
         options,
         this.jwk,
     ).update(content).final();
@@ -73,24 +70,31 @@ module.exports = class Client {
         rejectUnauthorized = true;
       }
       const httpsAgent = new https.Agent({
-        rejectUnauthorized, // Turn off ssl verification
+        rejectUnauthorized: rejectUnauthorized, // Turn off ssl verification
       });
-      const assertion = this.generateClientAssertion(tokenEndpoint);
-      return axios.create({
-        httpsAgent,
-        baseURL: tokenEndpoint,
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-      }).post('', assertion).then((response) => {
-        this.setBearerToken(response.data.access_token);
+
+      return this.generateClientAssertion(tokenEndpoint).then((assertion) => {
+        return axios.create({
+          httpsAgent,
+          baseURL: tokenEndpoint,
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+        }).post('', querystring.stringify(assertion)).then((response) => {
+          const json = ((typeof response.data) === 'string') ?
+            JSON.parse(response.data) :
+            response.data;
+          this.setBearerToken(json.access_token);
+        }).catch((e) => {
+          console.error(e);
+        });
       });
     });
   }
 
   processMessage(message) {
     // TODO: hook some local validation in here
-    return this.apiGateway.post('/R4/$process-message', message);
+    return this.apiGateway.post('/$process-message', message);
   };
 };
